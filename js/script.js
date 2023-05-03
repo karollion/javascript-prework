c4586d72d059455588f88c2a38c7ea5abd483616
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-05-03 13:03:54 +0200

    Add player move

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = `nieznany ruch`;
+let playerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
     computerMove = 'kamień';
@@ -12,3 +13,16 @@ if(randomNumber == 1){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if(playerInput == 2) {
+    playerMove = 'papier';
+} else {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
